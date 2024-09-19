2ac10c0f4ec83ddbfa6a77eec6c82b9ef721be20
XwX5855 <vancexu@foxmail.com>
2024-09-19 17:44:51 -0400

    Add files via upload

---
+++ starter_folder-main-master/starter_folder-main-master/scripts/05-replications.R
@@ -0,0 +1,19 @@
+#### Preamble ####
+# Purpose: Replicated graphs from... [...UPDATE THIS...]
+# Author: Wenxin Xu
+# Date: 19 February 2024
+# Contact: vance.xu@utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# Any other information needed?
+
+
+#### Workspace setup ####
+library(tidyverse)
+# [...UPDATE THIS...]
+
+#### Load data ####
+# [...ADD CODE HERE...]
+
+
+
